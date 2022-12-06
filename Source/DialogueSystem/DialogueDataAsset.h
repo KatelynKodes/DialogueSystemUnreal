478d9247f5c78df420081252ee8b1da5ab446a09
KatelynKodes <katelynwest225@gmail.com>
2022-12-06 12:44:16 -0600

    Created a Dialogue Asset Script, parents from a Data Asset Script

---
+++ Source/DialogueSystem/DialogueDataAsset.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DialogueDataAsset.generated.h"
+
+USTRUCT()
+struct FLine
+{
+	GENERATED_USTRUCT_BODY()
+	
+	UPROPERTY(EditAnywhere)
+	FString SpeakerName;
+	
+	UPROPERTY(EditAnywhere)
+	FColor TextColor = FColor(0.0f, 0.0f, 0.0f, 255.0f);
+
+	UPROPERTY(EditAnywhere)
+	FString SpeakerText;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class DIALOGUESYSTEM_API UDialogueDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere)
+	TArray<FLine> lines;
+};

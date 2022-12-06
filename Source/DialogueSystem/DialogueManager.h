e1bf45335a6b498e3489d731b1a768af79ab92bc
KatelynKodes <katelynwest225@gmail.com>
2022-12-06 12:59:53 -0600

    Created A dialogueManager actor

---
+++ Source/DialogueSystem/DialogueManager.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DialogueManager.generated.h"
+
+UCLASS()
+class DIALOGUESYSTEM_API ADialogueManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADialogueManager();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	void StartConversation()
+private:
+	void nextLine()
+	void endConversation();
+
+private:
+	TArray<FString> _lines;
+	new UDialogueDataAsset* _currentDialogue;
+	int _lineNum;
+};
